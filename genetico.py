1ed694057760dce8a38cb98793d4c6e29e8884fa
fernandeslara <almeida.larafern@gmail.com>
2026-06-01 20:00:43 -0300

    Completa o algoritmo geneticom com crossover e corrige mutação

--- genetico.py
+++ genetico.py
@@ -51,7 +51,8 @@ def selecao_torneio(populacao, k=3):
 
 import copy
 
-def mutacao(individuo):
+# Adicionei o parametro dist, para recalcular o fitness depois da mutação
+def mutacao(individuo, dist):
     rotas_validas = [r for r in individuo["rotas"] if len(r) > 3]
 
     if not rotas_validas:
@@ -62,5 +63,61 @@ def mutacao(individuo):
     idx1, idx2 = random.sample(range(1, len(rota) - 1), 2)
 
     rota[idx1], rota[idx2] = rota[idx2], rota[idx1]
+    
+    # Adicionei recalculo do fitness, pq ele tava ficando desatualizado
+    calcular_fitness(individuo, dist)
 
     return individuo
+
+def crossover(pai1, pai2, demandas, capacidade, dist):
+    seq1 = [c for rota in pai1["rotas"] for c in rota if c !=0]
+    seq2 = [c for rota in pai2["rotas"] for c in rota if c!= 0]
+
+    if tam < 2:
+        return copy.deepcopy(pai1)
+    
+    tam = len(seq1)
+    corte1, corte2 = sorted(random.sample(range(tam), 2))
+    filho_seq = [None] * tam
+    
+    for i in range(corte1, corte2 + 1):
+        filho_seq[i] = seq1[i]
+
+    pos = 0
+
+    for cliente in seq2:
+        if cliente not in filho_seq:
+            while filho_seq[pos] is not None:
+                pos += 1
+            filho_seq[pos] = cliente
+    
+    rotas = []
+    rota_atual = [0]
+    carga_atual = 0
+
+    for cliente in filho_seq:
+        demanda_cliente = demandas[cliente]
+        if carga_atual + demanda_cliente <= capacidade:
+            rota_atual.append(cliente)
+            carga_atual += demanda_cliente
+        
+        else:
+            rota_atual.append(0)
+            rotas.append(rota_atual)
+
+            rota_atual = [0, cliente]
+            carga_atual = demanda_cliente
+
+    rota_atual.append(0)
+    rotas.append(rota_atual)
+
+    filho = {
+        "S": None,
+        "rotas": rotas,
+        "fitness": None
+    }
+
+    calcular_fitness(filho, dist)
+
+    return filho
+
